c6947def8bab51d7b215f497d464442e325f8df2
CharlieMickey <charles.f.mickey@gmail.com>
2023-02-01 16:10:50 -0600

    establish initial website structure;

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const Nav = () => {
+  return ( 
+  <>
+    <Link to='/'> Home </Link>
+    <Link to='/about-me'> About Me </Link>
+  </> 
+  );
+}
+
+export default Nav;
